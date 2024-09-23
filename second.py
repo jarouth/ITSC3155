7c6559c1308b808992928a43b59b3ca82651fafa
jason routh <jasonrouth2005@gmail.com>
2024-09-23 15:14:26 -0400

    feat: added start of calculator class

---
+++ second.py
@@ -0,0 +1,13 @@
+class good_calculator:
+    def add(x,y):
+        return x+y
+    
+    def subtract(x,y):
+        return x-y
+    
+    def muliply(x,y):
+        return x*y
+    
+    def divide(x,y):
+        return x/y
+    
