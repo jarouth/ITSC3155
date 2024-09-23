358e15fc144ade31bea0f66cedcd34669d682675
jason routh <jasonrouth2005@gmail.com>
2024-09-23 16:44:11 -0400

    Feat: added calculator class, and formatted

---
+++ calculator.py
@@ -0,0 +1,36 @@
+def add(x, y):
+    print(f"{x}+{y} equals {x+y}")
+
+
+def subtract(x, y):
+    print(f"{x}-{y} equals {x-y}")
+
+
+def muliply(x, y):
+    print(f"{x}*{y} equals {x*y}")
+
+
+def divide(x, y):
+    print(f"{x}/{y} equals {x/y}")
+
+
+# Main function to ask user for input and then call the needed functions to run the calculator
+while (True):
+    number1 = int(input("Please input the first number:"))
+    number2 = int(input("Please input the second number:"))
+    choice = int(input(
+        "Please input \n 1 for addition \n 2 for subtraction \n 3 for multiplication \n 4 for divison: \n "))
+    if (choice == 1):
+        add(number1, number2)
+        break
+    elif (choice == 2):
+        subtract(number1, number2)
+        break
+    elif (choice == 3):
+        muliply(number1, number2)
+        break
+    elif (choice == 4):
+        divide(number1, number2)
+        break
+    else:
+        print("You have input an invalid number, please retry")
